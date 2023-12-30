a67b4d85c1b5d2f3d0207c0d88c845346b4a8660
andreasgiannoudis <agiannoudis@hotmail.com>
2023-12-30 18:07:07 +0100

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,42 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from '../pages/Home';
+import CreatePost from '../pages/CreatePost';
+import Login from '../pages/Login';
+import { useState } from 'react';
+import { signOut } from 'firebase/auth';
+import {auth} from "../components/firebase-config";
+
+
+export default function App(){
+    const [isAuth, setIsAuth] = useState(localStorage.getItem('isAuth'));
+    const signUserOut = () => {
+        signOut(auth).then(() => {
+            localStorage.clear();
+            setIsAuth(false);
+            window.location.pathname = '/login';
+        })
+    }
+    return(
+        <>  
+        <Router>
+            <nav>
+                <Link to="/">Home</Link>
+                
+                {!isAuth ? <Link to="/login">Login</Link> : 
+                <>
+                <Link to="/createpost">Create post</Link>
+                <button onClick={signUserOut}>Log out</button>
+                </>
+                }
+                
+            </nav>
+            <Routes>    
+                <Route path='/' element={<Home isAuth={isAuth} />} />
+                <Route path='/createpost' element={<CreatePost isAuth={isAuth} />} />
+                <Route path='/login' element={<Login setIsAuth={setIsAuth} />} />
+            </Routes>
+        </Router>
+
+        </>
+    )
+}
