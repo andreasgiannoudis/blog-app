1cec6b73b1e99f95e0066c09aa357ae1d3424a13
andreasgiannoudis <agiannoudis@hotmail.com>
2023-12-30 19:14:21 +0100

    fixing bugs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,49 @@
 import React from 'react';
 import { auth, provider } from '../components/firebase-config';
-import {signInWithPopup} from 'firebase/auth';
+import { signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
-function Login( {setIsAuth} ) {
 
-  let navigate = useNavigate();
+function generateRequestId() {
+  return Math.floor(Math.random() * Math.pow(10, 16));
+}
+
+function Login({ setIsAuth }) {
+  const navigate = useNavigate();
+  const cachedRequestId = localStorage.getItem('cachedRequestId');
+
+  const invalidateCachedRequestId = () => {
+    if (cachedRequestId) {
+      localStorage.removeItem('cachedRequestId');
+    }
+  };
 
   const signInWithGoogle = () => {
-     signInWithPopup(auth, provider).then((result) => {
+    const requestId = generateRequestId();
+
+    signInWithPopup(auth, provider).then((result) => {
       localStorage.setItem('isAuth', true);
       setIsAuth(true);
-      navigate("/");
-     })
-  }
+      navigate('/');
 
+      if (requestId !== cachedRequestId) {
+        invalidateCachedRequestId();
+      }
+    });
+  };
 
   return (
     <div className='LoginPage'>
       <p>Sign in with Google to continue</p>
-      <button className='login-with-google-btn' onClick={signInWithGoogle}>
+      <button
+        className='login-with-google-btn'
+        onClick={() => {
+          signInWithGoogle();
+        }}
+      >
         Sign in with Google
       </button>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
